f7be6bdbbe1cf6989f1647620117eb911cce3d31
transpobl65-cmyk <transpobl65@gmail.com>
2025-11-18 13:26:42 -0500

    cansada jefe

--- src/main/java/com/example/backend/controllers/test.java
+++ src/main/java/com/example/backend/controllers/test.java
@@ -1,6 +1,9 @@
 package com.example.backend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 
 public class test {
     @GetMapping("/test")
