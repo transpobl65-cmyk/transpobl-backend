91345125e3e7858f56e5302477f8793605c8b243
transpobl65-cmyk <transpobl65@gmail.com>
2025-11-20 16:39:50 -0500

    Add01

--- src/main/java/com/example/backend/util/CORS.java
+++ src/main/java/com/example/backend/util/CORS.java
@@ -25,7 +25,7 @@ public class CORS {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "https://transpobl-frontend.vercel.app",
+                                "https://transpobl-frontend.vercel.app/login",
                                 "http://localhost:4200"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
